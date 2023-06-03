263128884d21c337534228fa1d06671d0d401dc3
Gunarjith <129018798+Gunarjith@users.noreply.github.com>
2023-06-03 12:46:01 +0530

    Update views.py
    commit changes done by mohan

--- masterlink/views.py
+++ masterlink/views.py
@@ -21,7 +21,7 @@ def login(request):
         "customerPhone": "8494863493",
         # "returnUrl": "https://cashfree.com",
 
-         "notifyUrl": "https://verceldeploy-sigma.vercel.app",
+         "notifyUrl": "https://vailo.ai",
     }
 
     response = requests.request("POST", url, data=payload)
